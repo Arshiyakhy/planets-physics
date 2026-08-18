b9441e81ea7d897edbbc309aeda6987da4a19b0c
Arshiya Khayatzadeh <arshiyakhayatzadeh2006@gmail.com>
2026-08-17 22:25:40 -0400

    added plot

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from src.bodies import Body
-from src.physics import acceleration, step, energy, step_symplectic
 import numpy as np
+import matplotlib.pyplot as plt
+from src.bodies import Body
+from src.physics import acceleration, step, step_symplectic, energy
 
 sun = Body("Sun", mass=1.0, position=[0, 0], velocity=[0, 0])
-earth = Body("Earth", mass=3e-6, position=[1, 0], velocity=[0, 6.283])
+dt = 0.05
+steps = 1000
 
-a = acceleration(earth.position, sun.position, sun.mass)
-print(a)
-earth.position = np.array([1.0, 0.0])
-earth.velocity = np.array([0.0, 2*np.pi])
+# symplectic Euler
+earth = Body("Earth", mass=3e-6, position=[1.0, 0.0], velocity=[0.0, 2*np.pi])
+symplectic_positions = []
+for i in range(steps):
+    step(earth, sun.position, sun.mass, dt)
+    symplectic_positions.append(earth.position.copy())
+symplectic_positions = np.array(symplectic_positions)
 
-for i in range(1000):
-    step(earth, sun.position, sun.mass, 0.05)
-    if i % 20 == 0:
-        print(energy(earth, sun.position, sun.mass))
-earth = Body("Earth", 3e-6, [1.0, 0.0], [0.0, 2*np.pi])
-for i in range(1000):
-    step_symplectic(earth, sun.position, sun.mass, 0.05)
-    if i % 20 == 0:
-        print(energy(earth, sun.position, sun.mass))
-print(earth.position, earth.velocity)
+# forward Euler
+earth = Body("Earth", mass=3e-6, position=[1.0, 0.0], velocity=[0.0, 2*np.pi])
+forward_positions = []
+for i in range(steps):
+    step_symplectic(earth, sun.position, sun.mass, dt)
+    forward_positions.append(earth.position.copy())
+forward_positions = np.array(forward_positions)
+
+# Plot
+plt.figure(figsize=(8, 8))
+plt.plot(symplectic_positions[:, 0], symplectic_positions[:,
+         1], label="Symplectic Euler", linewidth=1)
+plt.plot(forward_positions[:, 0], forward_positions[:,
+         1], label="Forward Euler", linewidth=1)
+plt.plot(0, 0, 'yo', markersize=15, label="Sun")  # sun at origin
+plt.gca().set_aspect('equal')  # so orbits aren't visually distorted
+plt.xlabel("x (AU)")
+plt.ylabel("y (AU)")
+plt.title("Earth's orbit: Symplectic vs Forward Euler (dt=0.05)")
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.savefig("orbit_comparison.png", dpi=150)
+plt.show()
