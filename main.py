b2f7093eb2665ba41e0f69b475fdf38bfb3430a7
Arshiya Khayatzadeh <arshiyakhayatzadeh2006@gmail.com>
2026-08-17 22:01:13 -0400

    added step func

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from src.bodies import Body
-from src.physics import acceleration
+from src.physics import acceleration, step
 
 sun = Body("Sun", mass=1.0, position=[0, 0], velocity=[0, 0])
 earth = Body("Earth", mass=3e-6, position=[1, 0], velocity=[0, 6.283])
 
 a = acceleration(earth.position, sun.position, sun.mass)
 print(a)
+for _ in range(1000):
+    step(earth, sun.position, sun.mass, 0.001)
+
+print(earth.position, earth.velocity)
--- src/physics.py
+++ src/physics.py
@@ -7,3 +7,9 @@ def acceleration(pos, other_pos, other_mass):
     r_vec = pos - other_pos
     r_mag = np.linalg.norm(r_vec)
     return -G * other_mass * r_vec / r_mag**3
+
+
+def step(body, other_pos, other_mass, dt):
+    a = acceleration(body.position, other_pos, other_mass)
+    body.velocity += a * dt
+    body.position += body.velocity * dt
